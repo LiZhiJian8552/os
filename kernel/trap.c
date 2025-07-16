90c0a7df8c711a2ffeb321e806787423dedab96c
LiZhiJian <LiZhiJian8552@163.com>
2025-07-16 12:30:45 +0800

    update part trap annotation

--- kernel/trap.c
+++ kernel/trap.c
@@ -12,6 +12,7 @@ uint ticks;
 extern char trampoline[], uservec[], userret[];
 
 // in kernelvec.S, calls kerneltrap().
+// trap入口函数
 void kernelvec();
 
 extern int devintr();
@@ -32,40 +33,57 @@ trapinithart(void)
 //
 // handle an interrupt, exception, or system call from user space.
 // called from trampoline.S
-//
+//主要负责分发 trap 类型、保存/恢复上下文、调度进程。
 void
 usertrap(void)
 {
   int which_dev = 0;
 
+  // 检查当前trap是否来自用户态，r_sstatus()读取当前CPU的sstatus寄存器(保存当前特权级等信息)，SSTATUS_SPP表示trap发生前CPU的特权级
   if((r_sstatus() & SSTATUS_SPP) != 0)
     panic("usertrap: not from user mode");
 
   // send interrupts and exceptions to kerneltrap(),
   // since we're now in the kernel.
+  // 设置trap入口函数为kernelvec
   w_stvec((uint64)kernelvec);
 
+  // 获取当前进程的PCB
   struct proc *p = myproc();
   
   // save user program counter.
+  // 保存程序计数器即用户程序下一条指令的地址
+  // sepc即程序计数器寄存器
   p->trapframe->epc = r_sepc();
   
+  // 判断trap类型，8表示系统调用
   if(r_scause() == 8){
     // system call
 
+    // 如果当前进程被标记为killed，直接退出
     if(p->killed)
       exit(-1);
 
     // sepc points to the ecall instruction,
     // but we want to return to the next instruction.
+    // ?
     p->trapframe->epc += 4;
 
     // an interrupt will change sstatus &c registers,
     // so don't enable until done with those registers.
+    // 打开中断，允许内核被打断
     intr_on();
-
+    // 进入系统调用
     syscall();
-  } else if((which_dev = devintr()) != 0){
+    
+  } else if((which_dev = devintr()) != 0){ //调用 devintr() 判断是不是设备中断（比如时钟、磁盘、串口等）。
+    /**
+      devintr() 会判断当前 trap 是否是设备中断，并返回不同的值：
+
+        返回 2：表示是“时钟中断”（timer interrupt）
+        返回 1：表示是其它设备中断（如串口、磁盘等）
+        返回 0：不是设备中断
+    */
     // ok
   } else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
@@ -77,9 +95,12 @@ usertrap(void)
     exit(-1);
 
   // give up the CPU if this is a timer interrupt.
+  // 2表示时钟中断
   if(which_dev == 2)
+    // 让出CPU，调度其他进程
     yield();
 
+  // 准备返回用户态，恢复用户寄存器和页表等
   usertrapret();
 }
 
@@ -89,14 +110,17 @@ usertrap(void)
 void
 usertrapret(void)
 {
+  // 获取当前进程的PCB
   struct proc *p = myproc();
 
   // we're about to switch the destination of traps from
   // kerneltrap() to usertrap(), so turn off interrupts until
   // we're back in user space, where usertrap() is correct.
+  // 关闭中断，在恢复用户进程的过程中，内核不允许中断发生
   intr_off();
 
   // send syscalls, interrupts, and exceptions to trampoline.S
+  //设置 trap 入口为 trampoline.S 里的 uservec。
   w_stvec(TRAMPOLINE + (uservec - trampoline));
 
   // set up trapframe values that uservec will need when
@@ -119,12 +143,15 @@ usertrapret(void)
   w_sepc(p->trapframe->epc);
 
   // tell trampoline.S the user page table to switch to.
+  //生成用户页表的 satp 值，后续切换到用户页表。
   uint64 satp = MAKE_SATP(p->pagetable);
 
   // jump to trampoline.S at the top of memory, which 
   // switches to the user page table, restores user registers,
   // and switches to user mode with sret.
+  计算 trampoline.S 里的 userret 函数地址。
   uint64 fn = TRAMPOLINE + (userret - trampoline);
+  //userret 会切换到用户页表，恢复用户寄存器，然后执行 sret 指令，真正返回到用户态，继续运行用户程序。
   ((void (*)(uint64,uint64))fn)(TRAPFRAME, satp);
 }
 
